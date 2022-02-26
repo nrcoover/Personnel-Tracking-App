aadada08b5cf2f1e87a81451bcb6c7f0a44b1c09
nrcoover <81087345+nrcoover@users.noreply.github.com>
2022-02-26 09:46:51 -0500

    Created Permission Form UI
    Created the application page for reading/writing information into a task for an employee.

---
+++ PersonnelTracking/FrmPermissions.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace PersonnelTracking
+{
+    partial class FrmPermissions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEmployeeIDSearch = new System.Windows.Forms.TextBox();
+            this.lblEmployeeIDSearch = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dtpEmployeeBirthday = new System.Windows.Forms.DateTimePicker();
+            this.lblPermissionEnd = new System.Windows.Forms.Label();
+            this.blbPermissionStart = new System.Windows.Forms.Label();
+            this.lblDayAmount = new System.Windows.Forms.Label();
+            this.txtDayAmount = new System.Windows.Forms.TextBox();
+            this.lblPermissionDescription = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pnlPermissionsTop = new System.Windows.Forms.Panel();
+            this.pnlPermissionsBottom = new System.Windows.Forms.Panel();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.pnlPermissionsTop.SuspendLayout();
+            this.pnlPermissionsBottom.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtEmployeeIDSearch
+            // 
+            this.txtEmployeeIDSearch.BackColor = System.Drawing.Color.White;
+            this.txtEmployeeIDSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmployeeIDSearch.Location = new System.Drawing.Point(134, 28);
+            this.txtEmployeeIDSearch.Name = "txtEmployeeIDSearch";
+            this.txtEmployeeIDSearch.Size = new System.Drawing.Size(198, 26);
+            this.txtEmployeeIDSearch.TabIndex = 7;
+            // 
+            // lblEmployeeIDSearch
+            // 
+            this.lblEmployeeIDSearch.AutoSize = true;
+            this.lblEmployeeIDSearch.BackColor = System.Drawing.Color.Transparent;
+            this.lblEmployeeIDSearch.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmployeeIDSearch.ForeColor = System.Drawing.Color.Gray;
+            this.lblEmployeeIDSearch.Location = new System.Drawing.Point(27, 31);
+            this.lblEmployeeIDSearch.Name = "lblEmployeeIDSearch";
+            this.lblEmployeeIDSearch.Size = new System.Drawing.Size(101, 20);
+            this.lblEmployeeIDSearch.TabIndex = 8;
+            this.lblEmployeeIDSearch.Text = "Employee ID";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(134, 131);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(198, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // dtpEmployeeBirthday
+            // 
+            this.dtpEmployeeBirthday.Location = new System.Drawing.Point(134, 105);
+            this.dtpEmployeeBirthday.Name = "dtpEmployeeBirthday";
+            this.dtpEmployeeBirthday.Size = new System.Drawing.Size(198, 20);
+            this.dtpEmployeeBirthday.TabIndex = 0;
+            // 
+            // lblPermissionEnd
+            // 
+            this.lblPermissionEnd.AutoSize = true;
+            this.lblPermissionEnd.BackColor = System.Drawing.Color.Transparent;
+            this.lblPermissionEnd.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPermissionEnd.ForeColor = System.Drawing.Color.Gray;
+            this.lblPermissionEnd.Location = new System.Drawing.Point(27, 131);
+            this.lblPermissionEnd.Name = "lblPermissionEnd";
+            this.lblPermissionEnd.Size = new System.Drawing.Size(74, 20);
+            this.lblPermissionEnd.TabIndex = 12;
+            this.lblPermissionEnd.Text = "End Date";
+            // 
+            // blbPermissionStart
+            // 
+            this.blbPermissionStart.AutoSize = true;
+            this.blbPermissionStart.BackColor = System.Drawing.Color.Transparent;
+            this.blbPermissionStart.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blbPermissionStart.ForeColor = System.Drawing.Color.Gray;
+            this.blbPermissionStart.Location = new System.Drawing.Point(27, 106);
+            this.blbPermissionStart.Name = "blbPermissionStart";
+            this.blbPermissionStart.Size = new System.Drawing.Size(83, 20);
+            this.blbPermissionStart.TabIndex = 13;
+            this.blbPermissionStart.Text = "Start Date";
+            // 
+            // lblDayAmount
+            // 
+            this.lblDayAmount.AutoSize = true;
+            this.lblDayAmount.BackColor = System.Drawing.Color.Transparent;
+            this.lblDayAmount.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDayAmount.ForeColor = System.Drawing.Color.Gray;
+            this.lblDayAmount.Location = new System.Drawing.Point(377, 31);
+            this.lblDayAmount.Name = "lblDayAmount";
+            this.lblDayAmount.Size = new System.Drawing.Size(99, 20);
+            this.lblDayAmount.TabIndex = 8;
+            this.lblDayAmount.Text = "Day Amount";
+            // 
+            // txtDayAmount
+            // 
+            this.txtDayAmount.BackColor = System.Drawing.Color.White;
+            this.txtDayAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDayAmount.Location = new System.Drawing.Point(484, 28);
+            this.txtDayAmount.Name = "txtDayAmount";
+            this.txtDayAmount.ReadOnly = true;
+            this.txtDayAmount.Size = new System.Drawing.Size(198, 26);
+            this.txtDayAmount.TabIndex = 7;
+            // 
+            // lblPermissionDescription
+            // 
+            this.lblPermissionDescription.AutoSize = true;
+            this.lblPermissionDescription.BackColor = System.Drawing.Color.Transparent;
+            this.lblPermissionDescription.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPermissionDescription.ForeColor = System.Drawing.Color.Gray;
+            this.lblPermissionDescription.Location = new System.Drawing.Point(27, 166);
+            this.lblPermissionDescription.Name = "lblPermissionDescription";
+            this.lblPermissionDescription.Size = new System.Drawing.Size(93, 20);
+            this.lblPermissionDescription.TabIndex = 8;
+            this.lblPermissionDescription.Text = "Description";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(134, 163);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(496, 243);
+            this.textBox1.TabIndex = 2;
+            // 
+            // pnlPermissionsTop
+            // 
+            this.pnlPermissionsTop.BackColor = System.Drawing.Color.LightGray;
+            this.pnlPermissionsTop.Controls.Add(this.lblEmployeeIDSearch);
+            this.pnlPermissionsTop.Controls.Add(this.txtEmployeeIDSearch);
+            this.pnlPermissionsTop.Controls.Add(this.lblDayAmount);
+            this.pnlPermissionsTop.Controls.Add(this.txtDayAmount);
+            this.pnlPermissionsTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlPermissionsTop.Location = new System.Drawing.Point(0, 0);
+            this.pnlPermissionsTop.Name = "pnlPermissionsTop";
+            this.pnlPermissionsTop.Size = new System.Drawing.Size(724, 79);
+            this.pnlPermissionsTop.TabIndex = 0;
+            // 
+            // pnlPermissionsBottom
+            // 
+            this.pnlPermissionsBottom.BackColor = System.Drawing.Color.LightGray;
+            this.pnlPermissionsBottom.Controls.Add(this.btnSave);
+            this.pnlPermissionsBottom.Controls.Add(this.btnClose);
+            this.pnlPermissionsBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlPermissionsBottom.Location = new System.Drawing.Point(0, 434);
+            this.pnlPermissionsBottom.Name = "pnlPermissionsBottom";
+            this.pnlPermissionsBottom.Size = new System.Drawing.Size(724, 100);
+            this.pnlPermissionsBottom.TabIndex = 14;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnSave.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.DimGray;
+            this.btnSave.Location = new System.Drawing.Point(209, 26);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(150, 49);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnClose.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.DimGray;
+            this.btnClose.Location = new System.Drawing.Point(365, 26);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(150, 49);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // FrmPermissions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(724, 534);
+            this.Controls.Add(this.pnlPermissionsBottom);
+            this.Controls.Add(this.pnlPermissionsTop);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dtpEmployeeBirthday);
+            this.Controls.Add(this.lblPermissionEnd);
+            this.Controls.Add(this.blbPermissionStart);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblPermissionDescription);
+            this.Name = "FrmPermissions";
+            this.Text = "Permissions";
+            this.pnlPermissionsTop.ResumeLayout(false);
+            this.pnlPermissionsTop.PerformLayout();
+            this.pnlPermissionsBottom.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEmployeeIDSearch;
+        private System.Windows.Forms.Label lblEmployeeIDSearch;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dtpEmployeeBirthday;
+        private System.Windows.Forms.Label lblPermissionEnd;
+        private System.Windows.Forms.Label blbPermissionStart;
+        private System.Windows.Forms.Label lblDayAmount;
+        private System.Windows.Forms.TextBox txtDayAmount;
+        private System.Windows.Forms.Label lblPermissionDescription;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel pnlPermissionsTop;
+        private System.Windows.Forms.Panel pnlPermissionsBottom;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
